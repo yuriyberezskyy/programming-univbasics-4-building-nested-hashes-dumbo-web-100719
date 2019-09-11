e66a849b39173694243a0eeb37c0a86232187927
Yuriy Berezskyy <bereza95@gmail.com>
2019-09-11 17:22:12 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,5 +1,5 @@
 def base_hash
-  monopoly = {:railroads => 1 }
+  monopoly = {:railroads => {} }
   
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
