613f9c691c21d08cec835a9d37f7dc04487d8cb2
Yuriy Berezskyy <bereza95@gmail.com>
2019-09-11 17:41:20 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -29,9 +29,9 @@ def monopoly_with_third_tier
        :rent_in_dollars => 
        {
          :one_piece_owned => 25,
-         :second_piece_owned => 50,
-         :third_piece_owned => 100,
-         :fourth_piece_owned => 200
+         :two_piece_owned => 50,
+         :three_piece_owned => 100,
+         :four_piece_owned => 200
        },
        :names => 
        {
