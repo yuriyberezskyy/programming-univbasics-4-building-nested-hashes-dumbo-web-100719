65e6377e5afcee12ece87aa8b0ab7811bb83056c
Yuriy Berezskyy <bereza95@gmail.com>
2019-09-11 17:19:24 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,5 +1,5 @@
 def base_hash
-  base_hash = {keys => {count = 1}
+  base_hash = {keys => {count = 1}}
   
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
